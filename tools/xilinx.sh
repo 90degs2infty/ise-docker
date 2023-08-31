8a55497f7dd2d9abe0893494f94d05a485bd49a8
90degs2infty <90degs2infty@posteo.org>
2023-08-31 10:15:37 +0200

    Extend launch script to simplify running containerized commands

--- tools/xilinx.sh
+++ tools/xilinx.sh
@@ -1,5 +1,60 @@
 #!/bin/bash
-LICENSE_PATH=$1
+
+help() {
+    echo
+    echo "Run a containerized xilinx ISE command"
+    echo
+    echo "Syntax: xilinx.sh [-h] -l <license file> -p <project dir> -- <xilinx command>"
+    echo
+    echo "Options:"
+    echo "-h                display this help message"
+    echo "-l <license file> pass license file to the launched command"
+    echo "-p <project dir>  mount the specified project directory for access inside the container"
+    echo
+    echo "                  The directory is mounted under '/workspace'. The specified directory"
+    echo "                  has to be specified either as absolute path or with a leading './'"
+    echo "                  (i.e. './foo/ instead of foo/)."
+    echo
+    echo "Positional commands:"
+    echo "<xilinx command>  xilinx command to launch"
+    echo
+    echo "Example:"
+    echo "xilinx.sh -l ~/.Xilinx/Xilinx.lic -p . -- impact /workspace"
+    echo
+}
+
+while getopts ":hl:p:-" option; do
+    case $option in
+        h) # print help
+            help
+            exit;;
+        l) # license file
+            LICENSE_PATH=$OPTARG;;
+        p) # project directory
+            PROJECT_DIR=$OPTARG;;
+        -) # separator -- to split off everything that should go into the container
+            break;;
+        ?) # invalid option
+            echo "Error: invalid option -${OPTARG}"
+            exit 1;;
+    esac
+done
+
+if [ ! -f "$LICENSE_PATH" ]
+then
+    echo "license file '$LICENSE_PATH' does not exist"
+    exit 1
+fi
+
+if [ ! -d "$PROJECT_DIR" ]
+then
+    echo "project directory '$PROJECT_DIR' does not exist"
+    exit 1
+fi
+
+shift $((OPTIND - 1))
+
+printf "command passed into the container %s\n$*\n"
 
 podman run \
     -it \
@@ -8,5 +63,6 @@ podman run \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $LICENSE_PATH:/root/.Xilinx/Xilinx.lic:ro \
+    -v $PROJECT_DIR:/workspace \
     xilinx-ise
     # -v $HOME/.Xauthority:/root/.Xauthority:ro \
